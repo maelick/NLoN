ab57479807a9fc54aa194dcbd4a5224fbf7eb5ed
Maëlick Claes <himself@maelick.net>
2024-07-23 14:35:20 +0200

    better details when test assertion fails

--- rust/src/features/regex.rs
+++ rust/src/features/regex.rs
@@ -101,7 +101,7 @@ mod tests {
     {
         for (input, expected) in inputs.iter().zip(expected.iter()) {
             let value = func(input);
-            assert_eq!(value, *expected, "Failed for input: {}", input);
+            assert_eq!(value, *expected, "Failed for input: {} (got {:?}, expected {:?})", input, value, expected);
         }
     }
 
